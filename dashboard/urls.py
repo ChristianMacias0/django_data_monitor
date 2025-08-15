578d129966013661b8e471c3db181f9fefd647cb
Christian Macias <cjavmaci@espol.edu.ec>
2025-08-15 02:35:42 -0500

    as

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from .views import  index, health_check
 
 urlpatterns = [
-    path('', index, name='dashboard'),
+    #path('', index, name='dashboard'),
     path('health/', health_check, name='health_check'),
 ]
