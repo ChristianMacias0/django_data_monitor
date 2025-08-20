75fb187216283ff5cd3019892aa3f0cdbd14d806
jorssanc <jorssanc@espol.edu.ec>
2025-08-20 09:38:17 -0500

    hola

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -27,11 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_ff2#2pi^f7c^o9n$z($t4-b8^1l_n=&tf26g-r6v%7_ov@ft('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ["https://*.up.railway.app",
-    "http://127.0.0.1:8000",
+ALLOWED_HOSTS = ['.up.railway.app','127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ["https://*.up.railway.app"
     ]
 
 
